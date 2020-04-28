32336d12499788a429f4fc4b39963cada92f8c87
jferro1337 <64095066+jferro1337@users.noreply.github.com>
2020-04-28 21:20:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,3 @@
-import plotly.graph_objects as go
-import pandas as pd
-import numpy as np
-path_datasets = 'ex1.xlsx'
-path_lesson = path_datasets + 'ex1/'
-ex1 = pd.read_excel(path_datasets + 'ex1.xlsx', sheet_name='aux2020')
-ex1_week = pd.read_excel(path_datasets + 'ex1.xlsx', sheet_name='aux2020', index_col=0)
-entidade_list = ex1_week.columns
-data_1 = [dict(type='scatter',
-             x=ex1_week.index,
-             y=ex1_week[entidade],
-             name=entidade)
-                            for entidade in entidade_list]
-layout_1 = dict(title=dict(
-                        text='Procedimentos Criados'
-                  ),
-                  xaxis=dict(title='Semana 2020'),
-                  yaxis=dict(title='Procedimentos Criados'))
-fig_1 = go.Figure(data=data_1, layout=layout_1)
-
-fig_1.show()
 
 
 
@@ -40,7 +19,7 @@ app.layout = html.Div(children=[
 
     dcc.Graph(
         id='example-graph',
-        figure=fig_1
+        figure=fig
     )
 ])
 
