d8c3cd437a976a215327b90b48b9e0de09ef29bd
jferro1337 <64095066+jferro1337@users.noreply.github.com>
2020-04-28 21:25:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,43 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objects as go
+import pandas as pd
+import numpy as np
+
+# Dataset 'Processing'
+
+df_emissions = pd.read_csv('emission_full.csv')
+
+df_emission_0 = df_emissions.loc[df_emissions['year']==2000]
+
+# Building our Graphs (nothing new here)
+
+data_choropleth = dict(type='choropleth',
+                       locations=df_emission_0['country_name'],  #There are three ways to 'merge' your data with the data pre embedded in the map
+                       locationmode='country names',
+                       z=np.log(df_emission_0['CO2_emissions']),
+                       text=df_emission_0['country_name'],
+                       colorscale='inferno',
+                       colorbar=dict(title='CO2 Emissions log scaled')
+                      )
+
+layout_choropleth = dict(geo=dict(scope='world',  #default
+                                  projection=dict(type='orthographic'
+                                                 ),
+                                  #showland=True,   # default = True
+                                  landcolor='black',
+                                  lakecolor='white',
+                                  showocean=True,   # default = False
+                                  oceancolor='azure'
+                                 ),
+                         
+                         title=dict(text='World Choropleth Map',
+                                    x=.5 # Title relative position according to the xaxis, range (0,1)
+                                   )
+                        )
+
+fig = go.Figure(data=data_choropleth, layout=layout_choropleth)
 
 
 
@@ -11,10 +51,10 @@ server = app.server
 
 
 app.layout = html.Div(children=[
-    html.H1(children='Procedimentos SaphetyGov'),
+    html.H1(children='My First DashBoard'),
 
     html.Div(children='''
-        Avaliação Semanal
+        Example of html Container
     '''),
 
     dcc.Graph(
